fa2912dd2222ffe4fcb5d48466f3ffd5aa625ea6
JonasS <jonass@dev.jsje.de>
2019-02-19 20:46:31 +0100

    SSH variant

---
+++ ssh/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+file=./.ssh/authorized_keys
+
+rm -f $file && touch $file && chown build-dev:build-dev $file
+
+oldifs=$IFS
+
+options="no-port-forwarding,no-x11-forwarding,no-agent-forwarding"
+IFS=,
+for k in $SSH_KEYS; do
+  echo "$options $k" >>$file
+done
+
+
+IFS=$oldifs
+
+echo $@
+exec $@
