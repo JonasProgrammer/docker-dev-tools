976b7cca7e314a3a7579ea5346dfdc07bb794157
JonasS <jonass@dev.jsje.de>
2020-07-26 13:53:35 +0200

    Move towards github actions

--- entrypoint.ssh.sh
+++ entrypoint.ssh.sh
@@ -5,16 +5,13 @@ file=./.ssh/authorized_keys
 
 rm -f $file && touch $file && chown build-dev:build-dev $file
 
-oldifs=$IFS
-
 options="no-port-forwarding,no-x11-forwarding,no-agent-forwarding"
 IFS=,
 for k in $SSH_KEYS; do
   echo "$options $k" >>$file
 done
 
-
-IFS=$oldifs
+unset IFS
 
 echo $@
 exec $@
