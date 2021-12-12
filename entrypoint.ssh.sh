2a26fd3015e94a420bc7918e4be638658988a7cb
JonasS <jonass@dev.jsje.de>
2021-12-12 01:29:41 +0100

    Update tools and dist; fix unsafe known hostkeys; minor fixes

--- entrypoint.ssh.sh
+++ entrypoint.ssh.sh
@@ -13,5 +13,21 @@ done
 
 unset IFS
 
+if [ ! -f /etc/ssh/ssh_host_keys_flag ]; then
+  echo "Recreating SSH host keys (touch /etc/ssh/ssh_host_keys_flag to disable)..."
+  rm -f /etc/ssh/ssh_host_rsa_key /etc/ssh/ssh_host_ecdsa_key /etc/ssh/ssh_host_ed25519_key
+
+  ssh-keygen -q -N "" -t rsa -f /etc/ssh/ssh_host_rsa_key
+  ssh-keygen -l -f /etc/ssh/ssh_host_rsa_key.pub
+
+  ssh-keygen -q -N "" -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
+  ssh-keygen -l -f /etc/ssh/ssh_host_ecdsa_key.pub
+
+  ssh-keygen -q -N "" -t ed25519 -f /etc/ssh/ssh_host_ed25519_key
+  ssh-keygen -l -f /etc/ssh/ssh_host_ed25519_key.pub
+
+  touch /etc/ssh/ssh_host_keys_flag
+fi
+
 echo $@
 exec $@
